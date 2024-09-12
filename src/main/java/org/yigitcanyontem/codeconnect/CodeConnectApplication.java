7c9fde34a00f3ff9deedd692a58c01d124d6ba19
Yiğit Can Yöntem <yigit.can.yontem@saritay.com>
2024-09-12 21:18:57 +0300

    project setup

---
+++ src/main/java/org/yigitcanyontem/codeconnect/CodeConnectApplication.java
@@ -0,0 +1,13 @@
+package org.yigitcanyontem.codeconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/yigitcanyontem/codeconnect/CodeConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yigitcanyontem.codeconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
