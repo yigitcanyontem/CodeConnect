94d5526b5005aef9875b75730fb70e5c8bf5a3c3
yigitcanyontem <yigit.can.yontem@saritay.com>
2024-09-16 21:32:25 +0300

    added cors to api gw

--- apigw/src/main/java/org/yigitcanyontem/apigw/ApiGwGlobalFilter.java
+++ apigw/src/main/java/org/yigitcanyontem/apigw/ApiGwGlobalFilter.java
@@ -9,10 +9,12 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 public class ApiGwGlobalFilter implements GlobalFilter, Ordered {
@@ -37,19 +39,6 @@ public class ApiGwGlobalFilter implements GlobalFilter, Ordered {
                 .build());
     }
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("*");
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.addAllowedHeader("X-Requested-With");
-        configuration.addAllowedHeader("Content-Type");
-        configuration.addAllowedHeader("Authorization");
-        configuration.addAllowedHeader("x-csrf-token");
-        source.registerCorsConfiguration("/**", configuration.applyPermitDefaultValues());
-        return source;
-    }
 
     @Override
     public int getOrder() {
